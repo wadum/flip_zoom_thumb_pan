26d7ebc8553ff3de38d80ed5be9ac90e6e4d2949
Chris <chrwadum@gmail.com>
2015-03-13 14:49:17 +0100

    Kan zoome med flicks og panne med tommelfinger. Mangler at fixe billedet når man zoomer ud, man kan blive efterladt med en tom skærm.

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -12,6 +12,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+using Windows.Devices.Sensors;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -22,11 +27,136 @@ namespace App1
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        TransformGroup _transformGroup;
+        MatrixTransform _previousTransform;
+        CompositeTransform _compositeTransform;
+        Accelerometer _accelerometer;
+        Gyrometer _gyrometer;
+        uint _desiredReportInterval = 100;
+        private double _scaleFactor = 0.2;
+        private double _zoomThreshold = 0.15;
+
+
         public MainPage()
         {
             this.InitializeComponent();
 
-            this.NavigationCacheMode = NavigationCacheMode.Required;
+            ImageCanvas.ManipulationStarting += new ManipulationStartingEventHandler(ManipulateMe_ManipulationStarting);
+            ImageCanvas.ManipulationStarted += new ManipulationStartedEventHandler(ManipulateMe_ManipulationStarted);
+            ImageCanvas.ManipulationDelta += new ManipulationDeltaEventHandler(ManipulateMe_ManipulationDelta);
+            ImageCanvas.ManipulationCompleted += new ManipulationCompletedEventHandler(ManipulateMe_ManipulationCompleted);
+            ImageCanvas.ManipulationInertiaStarting += new ManipulationInertiaStartingEventHandler(ManipulateMe_ManipulationInertiaStarting);
+
+            InitManipulationTransforms();
+            InitAccelerometer();
+            InitGyrometer();
+        }
+
+        private void InitGyrometer()
+        {
+            _gyrometer = Gyrometer.GetDefault();
+            _gyrometer.ReportInterval = _desiredReportInterval;
+
+            //_gyrometer.ReadingChanged += new TypedEventHandler<Gyrometer, GyrometerReadingChangedEventArgs>(GyroReadingChanged);
+        }
+
+        private void InitManipulationTransforms()
+        {
+            _transformGroup = new TransformGroup();
+            _compositeTransform = new CompositeTransform();
+            _previousTransform = new MatrixTransform() { Matrix = Matrix.Identity };
+
+            _transformGroup.Children.Add(_previousTransform);
+            _transformGroup.Children.Add(_compositeTransform);
+
+            ManipulateImage.RenderTransform = _transformGroup;
+        }
+
+        private void InitAccelerometer()
+        {
+            _accelerometer = Accelerometer.GetDefault();
+            _accelerometer.ReportInterval = _desiredReportInterval;
+
+            _accelerometer.ReadingChanged += new TypedEventHandler<Accelerometer, AccelerometerReadingChangedEventArgs>(AccelReadingChanged);
+        }
+
+        async private void AccelReadingChanged(object sender, AccelerometerReadingChangedEventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                AccelerometerReading reading = e.Reading;
+                if (reading.AccelerationY > _zoomThreshold)
+                {
+                    GyrometerReading gr = _gyrometer.GetCurrentReading();
+                    Debug.WriteLine("Accelerometer x: " + reading.AccelerationX + ", y: " + reading.AccelerationY + ", z: " + reading.AccelerationZ);
+                    Debug.WriteLine("Gyrometer x: " + gr.AngularVelocityX + ", y: " + gr.AngularVelocityY + ", z: " + gr.AngularVelocityZ);
+                    if (gr.AngularVelocityX > 0)
+                    {
+                        _compositeTransform.ScaleX = _compositeTransform.ScaleY += _scaleFactor;
+                    }
+                    else
+                    {
+                        _compositeTransform.ScaleX = _compositeTransform.ScaleY -= _scaleFactor;
+                    }
+                }
+            });
+        }
+
+
+        void ManipulateMe_ManipulationStarting(object sender, ManipulationStartingRoutedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        void ManipulateMe_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        void ManipulateMe_ManipulationInertiaStarting(object sender, ManipulationInertiaStartingRoutedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        void ManipulateMe_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+
+            _previousTransform.Matrix = _transformGroup.Value;
+
+            Point center = _previousTransform.TransformPoint(new Point(e.Position.X, e.Position.Y));
+            _compositeTransform.CenterX = center.X;
+            _compositeTransform.CenterY = center.Y;
+
+            double x = e.Delta.Translation.X;
+            double y = e.Delta.Translation.Y;
+
+            double scale = _transformGroup.Value.M11;
+
+            GeneralTransform trans = ManipulateImage.TransformToVisual(null);
+            Rect rect = trans.TransformBounds(new Rect(0, 0, ManipulateImage.ActualWidth, ManipulateImage.ActualHeight));
+
+            // Prevent panning when reaching left and right
+            if ((rect.Left + x > 0  && x > 0) || (rect.Right - ActualWidth + x < 0 && x < 0))
+            {
+                x = 0;
+            }
+
+            // Prevent panning when reaching top and bottom
+            if ((rect.Top + y > 0 && y > 0) || (rect.Bottom - ActualHeight + y < 0 && y < 0))
+            {
+                y = 0;
+            }
+
+            _compositeTransform.ScaleX = _compositeTransform.ScaleY = e.Delta.Scale;
+            _compositeTransform.TranslateX = x;
+            _compositeTransform.TranslateY = y;
+
+            e.Handled = true;
+        }
+
+        void ManipulateMe_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            e.Handled = true;
         }
 
         /// <summary>
